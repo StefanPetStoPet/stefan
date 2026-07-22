512f1dcf982183ff1082a7cea43906d98d1d9096
Stefan K <stefankp505@gmail.com>
2026-07-22 21:54:36 +0200

    changed requestjs

--- i18n/request.js
+++ i18n/request.js
@@ -1,7 +1,9 @@
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async ({ locale }) => {
+  const currentLocale = locale ?? "me";
+
   return {
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${currentLocale}.json`)).default,
   };
 });
